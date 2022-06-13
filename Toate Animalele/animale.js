c0494469734616dbee06fe44d427fedf7e3d7244
TudFilip <filiptudormihail@yahoo.com>
2022-06-13 21:33:40 +0300

    creare butoane filtre

--- Toate Animalele/animale.js
+++ Toate Animalele/animale.js
@@ -1,3 +1,8 @@
 function functieButonSortareAnimale() {
     document.getElementById("optiuni-sortare-buton").classList.toggle("show");
 }
+
+
+function filtruClickEvent(el) {
+    console.log(el.parentElement.classList.toggle("show"));
+}
