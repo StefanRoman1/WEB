23b00b68aed67a4a31cc0b5a1e89a2a778c51fb1
TudFilip <filiptudormihail@yahoo.com>
2022-06-14 15:20:29 +0300

    multe modificari peste tot la pagina cu animale

--- Toate Animalele/animale.js
+++ Toate Animalele/animale.js
@@ -2,7 +2,39 @@ function functieButonSortareAnimale() {
     document.getElementById("optiuni-sortare-buton").classList.toggle("show");
 }
 
+function restrangereOptiuniSortare() {
+    document.getElementById("optiuni-sortare-buton").classList.toggle("show");
+}
 
 function filtruClickEvent(el) {
-    console.log(el.parentElement.classList.toggle("show"));
+    el.parentElement.classList.toggle("show");
+}
+
+
+function placeAnimalName(el) {
+    
+}
+
+
+let lastGrid = "grid-patrate";
+
+function changeIfDifferent(el, id) {
+    if (el.id === id && lastGrid !== id) {
+        const gridLinieElement = document.getElementById("grid-linie");
+        const gridPatrateElement = document.getElementById("grid-patrate");
+
+        lastGrid = id;
+        gridLinieElement.classList.toggle("show");
+        gridPatrateElement.classList.toggle("show");
+        gridChangedEvent();
+    }
+}
+
+function gridClickEvent(el) {
+    changeIfDifferent(el, "grid-linie");
+    changeIfDifferent(el, "grid-patrate");
+}
+
+function gridChangedEvent() {
+    let gridCurent = lastGrid; // poate fi "grid-linie" sau "grid-patrate", in functie de ce s-a selectat
 }
